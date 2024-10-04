1286404f7a1e89367b38c19d3f9ffa4550a806a2
Rehan alam <alamrehan12345678@gmail.com>
2024-10-04 20:29:20 +0530

    pushing react app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import './App.css'
-import TodoList from './TodoList'
-function App() {
+import React, { useState } from 'react';
+import './ToggleSwitch.css'; // Import CSS file for styling
+
+const ToggleSwitch = () => {
+  const [isOn, setIsOn] = useState(false);
+
+  const toggleSwitch = () => {
+    setIsOn(!isOn);
+  };
 
   return (
-    <>
-     <TodoList/>
-    </>
-  )
-}
+    <div className="toggle-switch-container">
+      <div className={`toggle-switch ${isOn ? 'on' : 'off'}`} onClick={toggleSwitch}>
+        <div className="toggle-switch-inner"></div>
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default ToggleSwitch;
